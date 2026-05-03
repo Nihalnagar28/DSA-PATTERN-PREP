2128f6bf34f9441809b542eafc18c9b82e4301d1
Nihal <nihalop28@gmail.com>
2026-05-03 17:07:37 +0530

    solved Longest Substring with K Uniques on GFG using sliding window approach

---
+++ Sliding-window/Max Sum Subarray of size K.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public: 
+    int maxSubarraySum(vector<int>& arr, int k) {
+        // code here
+        int low=0,high=k-1;
+        
+        int sum=0;
+        for(int i=low;i<=high;i++){
+            sum+=arr[i];
+        }
+        
+        int res=0;
+        while(high<arr.size()){
+            res=max(sum,res);
+            low++;
+            high++;
+            
+            if(high==arr.size())break;
+            
+            sum=sum-arr[low-1];
+            sum=sum+arr[high];
+            
+        }
+        
+        return res;
+    }
+};
