46fd1092a250c872d022b51e72978527992c8ccb
Nihal <nihalop28@gmail.com>
2026-05-20 10:32:18 +0530

    solved a leetcode daily problem using hashing 2657. Find the Prefix Common Array of Two Arrays

---
+++ Two-Pointers/2540. Minimum Common Value.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+       int n=nums1.size();
+       int m=nums2.size();
+       int i=0,j=0;
+       while(i<n && j<m){
+            if(nums1[i]==nums2[j])return nums1[i];
+            if(nums1[i]<nums2[j])i++;
+            else if(nums2[j]<nums1[i])j++;
+       }
+         return -1;
+    }
+};
