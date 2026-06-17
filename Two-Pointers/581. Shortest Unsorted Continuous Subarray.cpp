331e74643bc2187708f7137ef2df0577ad9492ce
Nihal <nihalop28@gmail.com>
2026-06-17 16:19:33 +0530

    solved 56. Merge Intervals on leetcode using merge intervals

--- Two-Pointers/581. Shortest Unsorted Continuous Subarray.cpp
+++ Two-Pointers/581. Shortest Unsorted Continuous Subarray.cpp
@@ -1,10 +1,22 @@
-#include <iostream>
+581. Shortest Unsorted Continuous Subarray
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& nums) {
+        int n=nums.size();
+        if(is_sorted(nums.begin(),nums.end())){
+            return 0;
+        }
+        int low=0,high=n-1;
+        while(low<n-1&&nums[low+1]>=nums[low])low++;
+        while(high>0&&nums[high-1]<=nums[high])high--;
+    int mini=INT_MAX,maxi=INT_MIN;
+        for(int i=low;i<=high;i++){
+            mini=min(mini,nums[i]);
+            maxi=max(maxi,nums[i]);
+        }
 
-using namespace std;
-
-int main() {
-    
-
-
-    return 0;
-}
+        while(low>0&&nums[low-1]>mini)low--;
+        while(high<n-1&&nums[high+1]<maxi)high++;
+        return high-low+1;
+    }
+};
