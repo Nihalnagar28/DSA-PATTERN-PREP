5809063b6f877717b2d75dd85e9e9a15799b8c16
Mritunjay nagar <mritunjaynihalnagar@gmail.com>
2026-08-10 12:34:06 +0530

    Time: 0 ms (100%), Space: 13.3 MB (80.02%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        int low=0,high=m-1;
+        int row=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(matrix[mid][0]<=target){
+                row=mid;
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        if(row==-1)return false;
+
+        low=0,high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(matrix[row][mid]==target)return true;
+            else if(matrix[row][mid]<target){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return false;
+    }
+};
