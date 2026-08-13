12ca378a1b197ad4d8519e9063f23901ffcaf853
Mritunjay nagar <mritunjaynihalnagar@gmail.com>
2026-08-13 15:37:13 +0530

    Time: 0 ms (100%), Space: 17 MB (95.43%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,14 +1,34 @@
 class Solution {
 public:
-    int kthSmallest(vector<vector<int>>& matrix, int k) {
-        int n=matrix.size();
-        vector<int>nom;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                nom.push_back(matrix[i][j]);
+    int func(vector<vector<int>>&matrix,int n,int guess){
+        int row=n-1;
+        int col=0;
+        int cnt=0;
+        while(row>=0&&col<n){
+            int rc=matrix[row][col];
+            if(rc<=guess){
+                cnt+=row+1;
+                col++;
+            }else{
+                row--;
             }
         }
-        sort(nom.begin(),nom.end());
-        return nom[k-1];
+        return cnt;
+    }
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n=matrix.size();
+        int low=matrix[0][0],high=matrix[n-1][n-1];
+        int res=0;
+        while(low<=high){
+            int guess=low+(high-low)/2;
+            int fun=func(matrix,n,guess);
+            if(fun<k){
+                low=guess+1;
+            }else{
+                res=guess;
+                high=guess-1;
+            }
+        }
+        return res;
     }
 };
