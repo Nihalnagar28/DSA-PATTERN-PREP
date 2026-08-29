92d8ffc922279ed8f0943e9df9b2ee2f28eced18
Mritunjay nagar <mritunjaynihalnagar@gmail.com>
2026-08-29 11:10:57 +0530

    Time: 67 ms (91.28%), Space: 131.5 MB (50.54%) - LeetHub

---
+++ 0502-ipo/0502-ipo.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        int n=profits.size();
+        vector<pair<int,int>>projects;
+        for(int i=0;i<n;i++){
+            projects.push_back({capital[i],profits[i]});
+        }
+        priority_queue<int>pq;
+        int idx=0;
+        sort(projects.begin(),projects.end());
+        while(k--){
+            while(idx<n){
+                if(projects[idx].first>w){
+                    break;
+                }
+
+                pq.push(projects[idx].second);
+                idx++;
+            }
+            if(pq.empty())return w;
+            w+=pq.top();
+            pq.pop();
+        }
+        return w;
+    }
+};
